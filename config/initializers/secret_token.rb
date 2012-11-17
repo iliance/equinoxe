d28fed0dee1214afd738b13ec58e23853fc4dce5
Jean-Marc PILON <jeanmarc@iliance.fr>
2012-11-17 14:09:51 +0100

    Version initiale

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Equinoxe::Application.config.secret_token = '4b52567bcfe039aa51ff58a64a2dba79ad79238f62c7645b68fd0fe3c4df43a6155a989b7c3d5e8c7ba0331d3d18ae448d33ca2fb8b4943e12ed1108b152fbd9'
